654c837de1e48dc23cb841edaa3565b40ef3698b
Will Schmidt <will.schmidt@gmail.com>
2020-01-27 00:02:14 +1300

    Add additional items to schema for jobs

--- index.js
+++ index.js
@@ -2,8 +2,29 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const port = 3000
+const mongoose = require('mongoose')
 
-const jobs = ['job one','job 2','job 3']
+mongoose.connect('mongodb+srv://dbUser:zu4UwRl9vEvuIDym@job-mgmt-clustor-zbi27.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true})
+
+
+const jobSchema = new mongoose.Schema({
+  name: String,
+  client: String
+});
+
+jobSchema.add({
+  type: String,
+  responsible: String,
+  cost: Number,
+  value: Number,
+  eta: Date,
+  days: Number,
+  status: String,
+  notes: String
+})
+
+const Job = mongoose.model('Job', jobSchema);
+
 
 app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
@@ -11,11 +32,16 @@ app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/jobs', (req, res) => {
-  res.send(jobs)
+  Job.find().then((data) => res.send(data))
+
+
+
+
 })
 
 app.post('/add-job', (req, res) => {
-  jobs.push(req.body)
+  const job = new Job({ name: req.body.name, client: req.body.client });
+  job.save().then(() => console.log('New job added'));
   res.sendStatus(200)
 }
 

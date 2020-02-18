fad3512d561395bb792d5829f620c3d07aed88c5
Will Schmidt <will.schmidt@gmail.com>
2020-02-19 08:03:05 +1300

    Latest changes

--- index.js
+++ index.js
@@ -4,15 +4,17 @@ const bodyParser = require('body-parser')
 const port = 3000
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://dbUser:zu4UwRl9vEvuIDym@job-mgmt-clustor-zbi27.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true})
-
+mongoose.connect(
+  'mongodb+srv://dbUser:zu4UwRl9vEvuIDym@job-mgmt-clustor-zbi27.mongodb.net/test?retryWrites=true&w=majority',
+  { useNewUrlParser: true }
+)
 
 const jobSchema = new mongoose.Schema({
   name: String,
   client: String
-});
+})
 
-JjobSchemaob.add({
+jobSchema.add({
   type: String,
   responsible: String,
   cost: Number,
@@ -23,8 +25,7 @@ JjobSchemaob.add({
   notes: String
 })
 
-const Job = mongoose.model('Job', jobSchema);
-
+const Job = mongoose.model('Job', jobSchema)
 
 app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
@@ -32,19 +33,30 @@ app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/jobs', (req, res) => {
-  Job.find().then((data) => res.send(data))
-
-
-
-
+  Job.find().then(data => res.send(data))
 })
 
 app.post('/add-job', (req, res) => {
-  const job = new Job({ name: req.body.name, client: req.body.client, value: req.body.value, type: req.body.type, responsible: req.body.responsible, cost: req.body.cost, eta: req.body.eta, days: req.body.days, status: req.body.status, notes: req.body.notes  })
-  job.save().then(() => console.log('New job added'))
-  res.sendStatus(200)
-}
-
-)
+  try {
+    console.log(req.body)
+    const job = new Job({
+      name: req.body.name,
+      client: req.body.client,
+      value: req.body.value,
+      type: req.body.type,
+      responsible: req.body.responsible,
+      cost: req.body.cost,
+      eta: req.body.eta,
+      days: req.body.days,
+      status: req.body.status,
+      notes: req.body.notes
+    })
+    job.save().then(() => console.log('New job added'))
+    res.sendStatus(200)
+  } catch (error) {
+    console.error(error.message)
+    res.sendStatus(500)
+  }
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))

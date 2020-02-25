960d969c489dfed58218bf4f71dfccc606e50098
Will Schmidt <will.schmidt@gmail.com>
2020-02-25 21:49:30 +1300

    Display all jobs in React

--- client/src/components/layout/Jobs.js
+++ client/src/components/layout/Jobs.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react'
 import axios from 'axios'
 
 export default class Jobs extends Component {
+  state = {
+    jobs: []
+  }
   async componentDidMount() {
     try {
       const res = await axios('http://localhost:5000/jobs')
       console.log(res.data)
+      this.setState({ jobs: res.data })
     } catch (error) {
       console.error("Couldn't connect to server " + error.message)
     }
@@ -15,6 +19,56 @@ export default class Jobs extends Component {
     return (
       <div className="container is-widescreen">
         <h1 className="title is-1 has-text-left">Jobs</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>
+                <abbr title="Job name">Job</abbr>
+              </th>
+              <th>
+                <abbr title="Client">Client</abbr>
+              </th>
+              <th>
+                <abbr title="Type of work">Type</abbr>
+              </th>
+              <th>
+                <abbr title="Staff resonsible">Respons.</abbr>
+              </th>
+              <th>
+                <abbr title="Estimated cost">Est. cost</abbr>
+              </th>
+              <th>
+                <abbr title="Billable amount">Billable</abbr>
+              </th>
+              <th>
+                <abbr title="ETA">ETA</abbr>
+              </th>
+              <th>
+                <abbr title="Days until due">Days</abbr>
+              </th>
+              <th>
+                <abbr title="Job status">Status</abbr>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.jobs.map(job => {
+              return (
+                <tr>
+                  <th>{job.name}</th>
+                  <td>{job.client}</td>
+                  <td>{job.type}</td>
+                  <td>{job.responsible}</td>
+                  <td>{job.cost}</td>
+                  <td>{job.value}</td>
+                  <td>{job.eta}</td>
+                  <td>{job.days}</td>
+                  <td>{job.status}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
       </div>
     )
   }

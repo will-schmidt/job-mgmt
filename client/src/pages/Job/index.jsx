bf96d772727a218255ed4709d027ed4fc0b25c1a
Will Schmidt <will.schmidt@gmail.com>
2020-03-05 17:30:01 +1300

    Add more details to job page

--- client/src/pages/Job/index.jsx
+++ client/src/pages/Job/index.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import moment from 'moment'
 
 export default class User extends Component {
   state = {
@@ -22,12 +23,35 @@ export default class User extends Component {
     if (!this.state.job) return null
     return (
       <div className="container is-widescreen">
-        <div className="card">
-          <div className="card-content">
-            <h1 className="title is-4 has-text-left">{this.state.job.name}</h1>
-            <p class="subtitle is-5 has-text-left">
-              {this.state.job.description}
-            </p>
+        <div class="columns">
+          <div class="column is-four-fifths">
+            <div className="card">
+              <div className="card-content">
+                <div className="level">
+                  <h1 className="title is-4 has-text-left">
+                    {this.state.job.name}
+                  </h1>
+                  <div className="level-right">
+                    <p className="subtitle is-5 has-text-left">
+                      <strong>Due:</strong> {moment(this.state.job.eta).format('D MMM YYYY')}
+                    </p>
+                  </div>
+                </div>
+
+                <p className="subtitle is-5 has-text-left">
+                  {this.state.job.description}
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="column">
+            <h2 className="subtitle is-5 has-text-left">
+              <strong>Client:</strong> {this.state.job.client.name}
+            </h2>
+
+            <h2 className="subtitle is-5 has-text-left">
+              <strong>Responsible:</strong> {this.state.job.responsible}
+            </h2>
           </div>
         </div>
       </div>
